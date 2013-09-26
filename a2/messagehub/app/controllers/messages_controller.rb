badf7932145d4208712c2c5a5ea49448bbf5d344
AJ Jenkins <aj.jenkins123@gmail.com>
2013-09-26 19:20:56 -0400

    Updated controller and views

--- a2/messagehub/app/controllers/messages_controller.rb
+++ a2/messagehub/app/controllers/messages_controller.rb
@@ -1,10 +1,18 @@
 class MessagesController < ApplicationController
+  protect_from_forgery except: :create
+  
+  respond_to :json
+  
   def index
+    @messages = Message.order("created_at ASC")
   end
 
   def show
+    @message = Message.find(params[:id])
   end
   
   def create
+    @message = Message.create(params[:message].permit(:content, :username, :app_id))
+    head :created, location: messages_path(@message)
   end
 end
--- a2/messagehub/config/routes.rb
+++ a2/messagehub/config/routes.rb
@@ -1,7 +1,7 @@
 Messagehub::Application.routes.draw do
-  get "messages/index"
+  get "messages/", to: "messages#index"
   get "messages/:id", to: "messages#show"
-  post "messages/create"
+  post "messages/", to: "messages#create"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
