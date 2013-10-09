2606e8add7f69c6cac4aa8dde0daf5423eb16421
AJ Jenkins <aj.jenkins123@gmail.com>
2013-10-09 14:37:04 -0400

    Added 2 validations to Message model. Completed after deadline, with Ming's approval

--- a2/messagehub/app/models/message.rb
+++ a2/messagehub/app/models/message.rb
@@ -1,7 +1,7 @@
 class Message < ActiveRecord::Base
   validates :app_id, presence: true
-  validates :content, presence: true
-  validates :username, presence: true
+  validates :content, presence: true, length: {maximum: 160}
+  validates :username, presence: true, length: {in: 3..60}
   validates :active, presence: true
   
   belongs_to :app
